2671a66a1a40ca3d059d24e00864198e6e2bc008
Sgilardone <sgilardone@mail.austral.edu.ar>
2024-09-03 08:25:23 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    year=int(input("Ingrese un año aqui:"))
+    if(year %4 == 0 and year %100 != 0) or (year %400 == 0):
+        print("El año es bisiesto")
+    else:
+        print("El año no es bisiesto")
